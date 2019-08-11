925c7974efd60a65fae3a727d9808c8d01a1baf0
Blac-Panda <oyefesotunmise@gmail.com>
2019-08-11 15:55:01 +0100

    Updates

---
+++ precache-manifest.3631017fcc3af70328d256f345656260.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39142bdaf92e1d29cc6ab80b7ee576fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d8bd5cd8569df41fa7f0",
+    "url": "/monsters-rolodex/static/css/main.7a086e6c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d8bd5cd8569df41fa7f0",
+    "url": "/monsters-rolodex/static/js/main.8370e456.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
